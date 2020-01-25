d494818eb9c302a3d7d6eae6bf2e0dee1d89fdeb
ocotaceb <32919835+ocotaceb@users.noreply.github.com>
2020-01-24 21:46:23 -0700

    Add files via upload
    Initial

---
+++ Woolftech_AI/Woolftech_AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Woolftech_AI
+//
+//  Created by Orlando Cota on 10/25/19.
+//  Copyright © 2019 Orlando Cota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
