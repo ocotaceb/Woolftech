42d46fff9ef724a51baabe2e75f7ea2d6fa5341d
ocotaceb <32919835+ocotaceb@users.noreply.github.com>
2020-02-05 13:22:40 -0700

    initial

---
+++ Woolftech/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Woolftech
+//
+//  Created by Orlando Cota on 2/5/20.
+//  Copyright © 2020 Orlando Cota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func fromNewAccount(segue: UIStoryboardSegue)
+    {
+        
+    }
+}
+
